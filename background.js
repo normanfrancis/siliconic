ca84263ec420e26a567ad61da49711514b066efb
Norman Francis <enormafrancis@gmail.com>
2017-12-07 14:14:00 -0500

    added function to toggle css

--- background.js
+++ background.js
@@ -1,10 +1,21 @@
+var cssApplied = false;
+
 function toggleCss(tab) {
 	function onError(error) {
 		console.log(error);
 	}
 
-	var insertingCSS = browser.tabs.insertCSS(tab.id, {file: "css/siliconic.css"});
-	insertingCSS.then(null, onError);
+	if (tab.url.includes("siliconera.com")) {
+		if (cssApplied) {
+			var removingCSS = browser.tabs.removeCSS(tab.id, {file: "css/siliconic.css"});
+			removingCSS.then(null, onError);
+			cssApplied = false;
+		} else {
+			var insertingCSS = browser.tabs.insertCSS(tab.id, {file: "css/siliconic.css"});
+			insertingCSS.then(null, onError);
+			cssApplied = true;
+		}
+	}
 }
 
 browser.browserAction.onClicked.addListener(toggleCss);
