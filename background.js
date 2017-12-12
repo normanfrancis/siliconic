0d3ec09764675594fa22bf8afe7a9aa5cd275205
Norman Francis <enormafrancis@gmail.com>
2017-12-11 21:22:03 -0500

    add all_urls permission so that extension will affect all tabs with siliconera on activation. added object to keep track of each tabs toggle state

--- background.js
+++ background.js
@@ -1,21 +1,32 @@
-var cssApplied = false;
-
 function toggleCss(tab) {
 	function onError(error) {
 		console.log(error);
 	}
 
 	if (tab.url.includes("siliconera.com")) {
-		if (cssApplied) {
+		if (tabStates[tab.id]) {
 			var removingCSS = browser.tabs.removeCSS(tab.id, {file: "css/siliconic.css"});
 			removingCSS.then(null, onError);
-			cssApplied = false;
+			tabStates[tab.id] = false;
 		} else {
+			console.log(tab);
 			var insertingCSS = browser.tabs.insertCSS(tab.id, {file: "css/siliconic.css"});
 			insertingCSS.then(null, onError);
-			cssApplied = true;
+			tabStates[tab.id] = true;
 		}
 	}
+
+	console.log(tabStates);
 }
+
+
+var gettingAllTabs = browser.tabs.query({});
+var tabStates = {};
+gettingAllTabs.then((tabs) => {
+	for (let tab of tabs) {
+		tabStates[tab.id] = false;
+		toggleCss(tab);
+	}
+});
 
 browser.browserAction.onClicked.addListener(toggleCss);
